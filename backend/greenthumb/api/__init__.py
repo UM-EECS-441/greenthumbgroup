d86e029a5a46bd2d2c2bf6158ae99e21e7b5f408
Achintya Kattemalavadi <akatt99@gmail.com>
2020-10-14 20:58:15 -0400

    Added the Flask folder/file structure

---
+++ backend/greenthumb/api/__init__.py
@@ -0,0 +1,11 @@
+"""
+
+GreenThumb REST API.
+
+GreenThumb Group <greenthumb441@umich.edu>
+
+"""
+
+# TODO: Do any universal api stuff.
+# Create .py files for any "parts" of the api and import here.
+# Basically, we'd set up our endpoints in the individual .py files.
---
+++ backend/greenthumb/views/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+GreenThumb views for web pages.
+
+GreenThumb Group <greenthumb441@umich.edu>
+
+"""
+
+# TODO: In case we need any views, seems like we only need api stuff atm.
+# Create .py files for views and import here.
