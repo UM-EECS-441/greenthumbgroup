f16d8e309eed9263b9a8dbd53975573f2ee615df
Achintya Kattemalavadi <akatt99@gmail.com>
2020-10-15 21:12:49 -0400

    put in skeleton code for some endpoints, issue #19

--- backend/greenthumb/api/__init__.py
+++ backend/greenthumb/api/__init__.py
@@ -6,6 +6,4 @@ GreenThumb Group <greenthumb441@umich.edu>
 
 """
 
-# TODO: Do any universal api stuff.
-# Create .py files for any "parts" of the api and import here.
-# Basically, we'd set up our endpoints in the individual .py files.
+from greenthumb import (catalog, errors, guides)
--- backend/greenthumb/models/__init__.py
+++ backend/greenthumb/models/__init__.py
@@ -1,10 +1,10 @@
 """
 
-GreenThumb Models.
+GreenThumb models.
 
 GreenThumb Group <greenthumb441@umich.edu>
 
 """
 
-# TODO: In case we need any models.
+# TODO: In case we need any models, not sure we do.
 # Create .py files for models and import here.
