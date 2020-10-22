ea73fa706db05615e923ef0ab9bf228160113821
jpriggs <jpriggs@umich.edu>
2020-10-21 23:04:49 -0400

    jriggs added account flow for storyboard and updated home page

---
+++ frontend/Green Thumb/Green Thumb/DesignableView.swift
@@ -0,0 +1,32 @@
+//
+//  DesignableView.swift
+//  Green Thumb
+//
+//  Created by Joe Riggs on 10/21/20.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
