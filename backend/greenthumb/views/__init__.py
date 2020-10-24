0a889e54b1ac9dd41573e02c78a1b6a425f6a501
shikikan-louyi <louyi@umich.edu>
2020-10-24 16:19:24 -0400

    add testpage #19

--- backend/greenthumb/views/__init__.py
+++ backend/greenthumb/views/__init__.py
@@ -8,3 +8,4 @@ GreenThumb Group <greenthumb441@umich.edu>
 
 # TODO: In case we need any views, seems like we only need api stuff atm.
 # Create .py files for views and import here.
+import greenthumb.views.testpage
