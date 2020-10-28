c9a13a73875b1ebb5186199961e20698916c6666
shikikan-louyi <louyi@umich.edu>
2020-10-27 20:32:05 -0400

    temporarily disable import of usergarden to fix it

--- backend/greenthumb/api/__init__.py
+++ backend/greenthumb/api/__init__.py
@@ -9,4 +9,4 @@ GreenThumb Group <greenthumb441@umich.edu>
 from greenthumb.api.catalog import (get_catalog, get_catalog_plant_page)
 from greenthumb.api.guides import(get_guides, get_guide_page)
 # import greenthumb.api.usergarden
-from greenthumb.api.usergarden import (get_user_gardens, get_garden)
+# from greenthumb.api.usergarden import (get_user_gardens, get_garden)
