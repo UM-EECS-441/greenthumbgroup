1c0777dc62ddcf40a500ce8ad3dcfc1764a1adbc
shikikan-louyi <louyi@umich.edu>
2020-10-27 20:23:48 -0400

    add missing endpoints for usergarden

--- backend/greenthumb/api/__init__.py
+++ backend/greenthumb/api/__init__.py
@@ -8,3 +8,4 @@ GreenThumb Group <greenthumb441@umich.edu>
 
 from greenthumb.api.catalog import (get_catalog, get_catalog_plant_page)
 from greenthumb.api.guides import(get_guides, get_guide_page)
+import greenthumb.api.usergarden
