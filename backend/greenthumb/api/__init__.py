54b7786260555a5d00bdc3f2d165b0638584f3ec
shikikan-louyi <louyi@umich.edu>
2020-10-27 20:27:41 -0400

    fix indentation issue in __init__.py

--- backend/greenthumb/api/__init__.py
+++ backend/greenthumb/api/__init__.py
@@ -8,4 +8,5 @@ GreenThumb Group <greenthumb441@umich.edu>
 
 from greenthumb.api.catalog import (get_catalog, get_catalog_plant_page)
 from greenthumb.api.guides import(get_guides, get_guide_page)
-import greenthumb.api.usergarden
+# import greenthumb.api.usergarden
+from greenthumb.api.usergarden import (get_user_gardens, get_garden)
