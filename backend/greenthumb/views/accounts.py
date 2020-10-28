9f54eb6568dfe1274fd25bd02abd39ba69c7e908
shikikan-louyi <louyi@umich.edu>
2020-10-27 20:20:57 -0400

    fix for 500 internal server errors in login #19

--- backend/greenthumb/views/accounts.py
+++ backend/greenthumb/views/accounts.py
@@ -28,7 +28,7 @@ def create_user():
                 users(email=request.json['email'], gardens=[]).save()
             session['email'] = request.json['email']
             # TODO: change based on function name
-            return redirect(url_for('user_gardens'))
+            return redirect(url_for('get_user_gardens'))
     # TODO: change this return
     return {}
 
@@ -36,14 +36,14 @@ def create_user():
 def login():
     if 'email' in session:
         # TODO: change based on function name
-        return redirect(url_for('user_gardens'))
+        return redirect(url_for('get_user_gardens'))
     if request.method == 'POST':
         with util.MongoConnect():
             for user in users.objects():
                 if request.json['email'] == user.email:
                     session['email'] = user['email']
                     # TODO: change based on function name
-                    return redirect(url_for('user_gardens'))
+                    return redirect(url_for('get_user_gardens'))
             return redirect(url_for('create_user'))
     # TODO: change this return
     return {}
