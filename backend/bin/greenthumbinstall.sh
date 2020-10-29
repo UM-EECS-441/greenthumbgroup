ff0b98352be9c2b5c1c1ae4a0abfe2c1b31ba7af
shikikan-louyi <louyi@umich.edu>
2020-10-29 11:48:11 -0400

    (tenative) finish server install script #19

--- backend/bin/greenthumbinstall.sh
+++ backend/bin/greenthumbinstall.sh
@@ -32,4 +32,23 @@ set -u
 echo "Installing required python packages..."
 pip install -e .
 
+echo "Installing MongoDB v4.4..."
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt update
+apt install mongodb-org
+systemctl start mongod.service
+systemctl enable mongod
+
 echo "Setting up gunicorn service..."
+cp scripts/greenthumb.service /etc/systemd/system/
+echo "Copying nginx config files..."
+cp scripts/greenthumb /etc/nginx/sites-available/
+echo "Enabling greenthumb in nginx..."
+ln -s /etc/nginx/sites-available/greenthumb /etc/nginx/sites-enabled/
+
+echo "Starting greenthumb..."
+systemctl enable greenthumb
+systemctl start greenthumb
+
+echo "Installation complete."
