8a7ab97fce5109fe84e171c4171d2920df3bda4c
shikikan-louyi <louyi@umich.edu>
2020-10-28 21:48:58 -0400

    experimental: work on install script

--- backend/bin/greenthumbinstall.sh
+++ backend/bin/greenthumbinstall.sh
@@ -5,12 +5,31 @@
 # GreenThumb Group <greenthumb441@umich.edu>
 
 set -Eeuo pipefail
-set -x
+# Uncomment for debugging purposes
+# set -x
 
+# check root status
+if [[ $(id -u) -ne 0 ]];
+then 
+    echo "Please run this script as root."
+    exit 1
+fi
+
+# check that we are in the main backend directory
+if [[ $(pwd | grep -c "backend/") -ne 0 ]]; then
+    echo "Please execute this script in the main backend/ directory."
+    exit 1
+fi
+
+echo "Creating python venv..."
 python3 -m venv env
 
+echo "Activating venv..."
 set +u
 source env/bin/activate
 set -u
 
+echo "Installing required python packages..."
 pip install -e .
+
+echo "Setting up gunicorn service..."
