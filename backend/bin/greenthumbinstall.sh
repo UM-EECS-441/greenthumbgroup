dddfe648db76eb1f22249c0c0f9b97597d9c9453
shikikan-louyi <louyi@umich.edu>
2020-10-29 18:31:50 -0400

    tweak install script to not require venv;

--- backend/bin/greenthumbinstall.sh
+++ backend/bin/greenthumbinstall.sh
@@ -21,14 +21,6 @@ if [[ $(pwd | grep -c "backend/") -ne 0 ]]; then
     exit 1
 fi
 
-echo "Creating python venv..."
-python3 -m venv env
-
-echo "Activating venv..."
-set +u
-source env/bin/activate
-set -u
-
 echo "Installing required python packages..."
 pip install -e .
 
